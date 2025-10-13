dbc486405d87df194769124bd9d89fd25b68904e
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-13 19:11:45 +0100

    changes made

--- src/pages/signinPage.jsx
+++ src/pages/signinPage.jsx
@@ -11,18 +11,18 @@ function SignIn() {
  
 
   return (
-    <div className="fixed top-[64px] right-[0px] gap-[17px] flex items-start bg-[#000000] h-screen w-[1365px]">
+    <div className="fixed top-[64px] right-[0px] gap-[45px] flex items-start bg-[#000000] h-screen w-[1365px]">
        <div>
         <img
-          className="w-[689px] w-[689px] h-full "
+          className="w-[560px] w-[689px] h-full "
           src={Images.signinImage}
           alt="sign in image"
         />
       </div>
-      <div className="w-[689px]  flex flex-col justify-center items-start ">
-        <div className="w-[559px] flex justify-between items-center ">
-          <p className="font-bold text-[32px] text-[#ffffff]">SignUp</p>
-          <p className="text-[#168DE1] font-medium text-[20px]">Login</p>
+      <div className="w-[689px] gap-[20px] mt-[40px] flex flex-col justify-center items-start ">
+        <div className="w-[350px] flex justify-between items-center ">
+          <p className="font-bold text-[16px] text-[#ffffff]">SignUp</p>
+          <p className="text-[#168DE1] font-medium text-[16px]">Login</p>
         </div>
         <div className=" ">
           <Input
@@ -45,7 +45,7 @@ function SignIn() {
           />
 
           <Button
-            size="md"
+            size="lg"
             variant="secondary"
             className="mt-[40px] mb-5 text-[#ffffff] rounded-[7px] "
             
@@ -59,10 +59,10 @@ function SignIn() {
           </div>
 
           <div className="flex justify-center mt-[15px] ">
-             <pre className="text-[14px] text-[#8c8c8c] font-light mt-[30px] w-[559px] ">
+             <p className="text-[14px] text-center text-[#8c8c8c] font-light mt-[30px] w-[300px] ">
                 By signing up, i agree to the wisper 
                 Terms & conditions and Privacy policy
-            </pre>
+            </p>
           </div>
           
         </div>
